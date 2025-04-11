1ce3c07e6b32c6a2abb193b0cfb4fe2929cf3899
FabioCavalcante1007 <fabiobcavalcante@gmail.com>
2025-04-11 11:46:13 -0300

    formulário de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { FiMail, FiLock } from 'react-icons/fi'; // Ícones
-import '../Login.css'; // Seu CSS personalizado
+import React from "react";
+import { FiMail, FiLock } from "react-icons/fi"; // Ícones
+import "../Login.css"; // Seu CSS personalizado
 
 const Login = () => {
   return (
     <div className="auth-layout">
       <div className="auth-content">
         {/* Logo */}
-        <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
+        <div style={{ textAlign: "center", marginBottom: "1rem" }}>
           <img src="/logo.png" alt="Logo" style={{ height: 60 }} />
         </div>
-
         <h2 className="text-center mb-4">Bem-vindo!!</h2>
-
         {/* Email */}
-        <label htmlFor="email">E-mail</label>
-        <div className="input-icon">
+        <label htmlFor="email">Email</label>
+        <div className="input-icon" style={{marginBottom: "1rem" }}>
           <FiMail className="icon" />
           <input type="email" id="email" placeholder="Digite seu e-mail" />
         </div>
-
         {/* Senha */}
         <label htmlFor="password">Senha</label>
         <div className="input-icon">
@@ -30,7 +27,9 @@ const Login = () => {
         <div className="remember-me">
           <input type="checkbox" id="remember" />
           <label htmlFor="remember"> Lembre-se de mim</label>
-          <a href="#" className="forgot">Esqueceu a senha?</a>
+          <a href="#" className="forgot">
+            Esqueceu a senha?
+          </a>
         </div>
 
         <button className="login-button">Entrar</button>
